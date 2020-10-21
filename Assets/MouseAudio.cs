73616e6d6cc0808d2398d1a6a526624998a7d2d8
bensuarez1 <G00292013@stu.raritanval.edu>
2020-10-21 11:34:49 -0400

    W6 Audio Project

---
+++ Assets/MouseAudio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioS;
+
+    public void PlayClip(AudioClip clip)
+    {
+        audioS.PlayOneShot(clip);
+    }
+}
